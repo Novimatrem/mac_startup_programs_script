26fe9c41c3b323e0b1202f1200fbdab76be8a644
Novimatrem <69379708+Novimatrem@users.noreply.github.com>
2026-08-02 12:39:17 +0100

    if this fixes it, it is whoami being not me

--- mac_startup_programs_script.sh
+++ mac_startup_programs_script.sh
@@ -41,8 +41,7 @@ fi
 #open "/Applications/Minecraft.app"
 
 # run mac_desktop_cleanliness without hanging up the terminal, so more can execute after this point, and/or we can exit cleanly without hanging.
-osascript -e 'display notification "whoami is $(whoami)" with title "mac_startup_programs_script.sh"'
-bash -c "sleep 0s && nohup bash /Users/$(whoami)/mac_desktop_cleanliness/mac_desktop_cleanliness.sh && rm -rf $HOME/nohup.out && rm -rf $(pwd)/nohup.out && rm -rf /opt/nohup.out && disown & disown"
+bash -c "sleep 0s && nohup bash /Users/zoeyglobe/mac_desktop_cleanliness/mac_desktop_cleanliness.sh && rm -rf $HOME/nohup.out && rm -rf $(pwd)/nohup.out && rm -rf /opt/nohup.out && disown & disown"
 echo ""
 sleep 1s
 echo ""
@@ -56,3 +55,4 @@ printf "\a"
 sleep 1s # it doesn't show unless i wait 1s here
 
 # EOF
+
